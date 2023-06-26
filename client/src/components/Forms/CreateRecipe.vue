108b1b28188c8d43fd9086cd2f9e55dfab5a83a9
Nico Gartmann <n.gartmann@bildungsinnovator.com>
2023-06-26 10:10:35 +0200

    RA-5_Schritte_umsortieren
    - Zubereitungsschritte können nun nach Erstellen umsortiert werden.
    - Die `stepNumber` wird nach umsortieren/löschen aktualisiert.

--- client/src/components/Forms/CreateRecipe.vue
+++ client/src/components/Forms/CreateRecipe.vue
@@ -34,16 +34,20 @@
     </v-row>
 
     <h2>Zubereitungsschritte</h2>
-    <v-text-field v-model="stepDescription" label="Schritt" append-inner-icon="mdi-plus" @click:append-inner="addStep"
+    <v-text-field v-model="stepDescriptionInput" label="Schritt" append-inner-icon="mdi-plus" @click:append-inner="addStep"
                   clearable/>
     <v-list>
-      <v-list-item border v-for="(step, index) in steps" :key="step.stepNumber" :title="step.description">
-        <template v-slot:append>
-          <v-btn icon @click="removeStep(index)">
-            <v-icon>mdi-trash-can-outline</v-icon>
-          </v-btn>
+      <draggable v-model="steps" item-key="stepNumber" @end="updateStepNumbers">
+        <template #item="{ element }">
+          <v-list-item :id="element.stepNumber" border :title="element.stepNumber" :subtitle="element.description">
+            <template v-slot:append>
+              <v-btn icon @click="removeStep(index)">
+                <v-icon>mdi-trash-can-outline</v-icon>
+              </v-btn>
+            </template>
+          </v-list-item>
         </template>
-      </v-list-item>
+      </draggable>
     </v-list>
     <v-btn-group>
       <v-btn color="red" prepend-icon="mdi-close">
@@ -58,6 +62,7 @@
 
 <script setup>
 import axios from 'axios'
+import draggable from 'vuedraggable'
 import {reactive, ref} from 'vue'
 
 const ingredient = reactive({
@@ -66,12 +71,18 @@ const ingredient = reactive({
   amount: ''
 });
 
-const stepDescription = ref();
+const stepDescriptionInput = ref();
 
 const name = ref();
 const description = ref();
 const ingredients = reactive([]);
-const steps = reactive([]);
+const steps = ref([]);
+
+const updateStepNumbers = () => {
+  steps.value.forEach((step, index) => {
+    step.stepNumber = index + 1;
+  })
+}
 
 function abort() {
 }
@@ -81,7 +92,7 @@ const save = () => {
     name: name.value,
     description: description.value,
     ingredients: ingredients,
-    steps: steps
+    steps: Array.from(steps.value)
   }
 
   axios.post("api/recipeList", recipe)
